d059b0584d6ff6ef4353b485c41ca668c0cbd60a
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2024-01-20 14:59:23 +0700

    Prac2.2 with full display of student and courses info but had trouble with displaying scores in a course

--- practical/2.student.mark.py
+++ practical/2.student.mark.py
@@ -9,17 +9,17 @@ class Student:
 
     def displayInfo(self):
         print(f"""Student ID: {self.id} 
-              Student name: {self.name} 
-              DOB: {self.dob} """)
+        Student name: {self.name} 
+        DOB: {self.dob} """)
 class Course:
-    def __init__(self, id, name, studentNumber):
-        self.id = id
+    def __init__(self, id, name):
+        self.__id = id
         self.name = name
         # self.studentNumber = studentNumber
     def displayInfo(self):
         print(f""" --------------
-              Course ID: {self.id} 
-              Course Name: {self.name}""")
+        Course ID: {self.__id} 
+        Course Name: {self.name}""")
         
 students = []
 courses = {}
@@ -30,3 +30,30 @@ for _ in range(studentsNumber):
     name = input("Student Name: ")
     dob = input("DOB: ")
     students.append(Student(id, name, dob))
+
+coursesNumber = int(input("Number of courses: "))
+for _ in range(coursesNumber):
+        code = input("Course ID: ")
+        name = input("Course Name: ")
+        courses[code] = name
+
+print("-------Score insertion-------")
+
+id = int(input("Student ID: "))
+course_code = input("Course ID: ")
+score = int(input("Score: "))
+for student in students:
+    if student.id == id:
+        student.setScore(course_code, score)
+
+
+for student in students:
+    student.displayInfo()
+
+for course in courses:
+    student.displayInfo()
+
+id = int(input("Student ID: "))
+for student in students:
+    if student.id == id:
+        print(f"Student {student.id} scored {student.score}")
